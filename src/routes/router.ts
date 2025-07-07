06a0685a46d8343619910b798a8d6b33dee6bbd3
Leider17 <leidersolano133@gmail.com>
2025-07-07 03:46:25 -0500

    the implementation of the routes was changed

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,35 +1,67 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
+import NotFound from "../views/NotFound.vue";
 import EditLegend from "../views/EditLegend.vue";
 import CreateLegend from "../views/CreateLegend.vue";
-import LegendDetails from "../views/LegendDetails.vue";
+import Login from "../views/Login.vue";
+import Register from "../views/Register.vue";
 
-const routes=[
+import { useAuthStore } from "../stores/authStore";
+
+const routes = [
     {
-        path:"/",
-        name:"Home",
-        component:Home
+        path: "/",
+        name: "Home",
+        component: Home,
+        meta: { requiresAuth: true }
     },
     {
-        path:"/edit/:id",
-        name:"EditLegend",
-        component:EditLegend
+        path: "/login",
+        name: "Login",
+        component: Login,
+        meta: { requiresAuth: false }
     },
     {
-        path:"/create",
-        name:"CreateLegend",
-        component:CreateLegend
+        path: "/register",
+        name: "Register",
+        component: Register,
+        meta: { requiresAuth: false }
     },
     {
-        path:"/legend/:id",
-        name:"LegendDetails",
-        component:LegendDetails
+        path: "/edit/:id",
+        name: "EditLegend",
+        component: EditLegend,
+        meta: { requiresAuth: true }
+    },
+    {
+        path: "/createLegend",
+        name: "CreateLegend",
+        component: CreateLegend,
+        meta: { requiresAuth: true }
+    },
+    {
+        path: "/:pathMatch(.*)*",
+        name: "NotFound",
+        component: NotFound,
+        meta: { requiresAuth: false }
     }
-]
+];
 
-const router= createRouter({
-    history:createWebHistory(),
+const router = createRouter({
+    history: createWebHistory(),
     routes
-})
+});
+
+router.beforeEach(async (to, from, next) => {
+    const authStore = useAuthStore();
+
+    if (to.meta.requiresAuth && !authStore.isAuthenticated) {
+        next({ name: 'Login' });
+    } else if ((to.name === 'Login' || to.name === 'Register') && authStore.isAuthenticated) {
+        next({ name: 'Home' });
+    } else {
+        next();
+    }
+});
 
 export default router;
