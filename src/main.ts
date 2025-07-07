2d93dd9eafa6f1ad8f186eb0c858144b4a586a26
Leider17 <leidersolano133@gmail.com>
2025-07-07 03:42:51 -0500

    added tailwind implementation and other dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,19 @@
+// src/main.ts
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './routes/router'
+import { createPinia } from 'pinia'
+import { useAuthStore } from './stores/authStore'
 
-createApp(App).use(router).mount('#app')
+const pinia = createPinia();
+const app = createApp(App);
+
+app.use(pinia);
+
+const authStore = useAuthStore(); 
+authStore.initializeAuth(); 
+                            
+
+
+createApp(App).use(router).use(pinia).mount('#app');
