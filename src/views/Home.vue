d9a2079993a9b35280eeaf0e2321b47fc613168b
Leider17 <leidersolano133@gmail.com>
2025-07-07 03:55:59 -0500

    main view and notfound are added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,4 +1,61 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted} from "vue";
+import { useLegendStore } from "../stores/legendStore";
+import HeaderComponent from "../components/layout/HeaderComponent.vue";
+import Filters from "../components/ui/Filters.vue";
+import CardLegend from "../components/ui/CardLegend.vue";
+import Loading from "../components/ui/Loading.vue";
+import Error from "../components/ui/Error.vue";
 
-<template>
+const legendStore = useLegendStore();
+
+onMounted(() => {
+  legendStore.fetchLegends();
+});
+
+
+
+async function handleFilters(filters: any) {
+  const params = new URLSearchParams()
+  for (const key in filters) {
+    if (filters[key]) {
+      params.append(key, filters[key])
+    }
+  }
+  const filtersObject = Object.fromEntries(params.entries());
+  try {
+     await legendStore.fetchLegendsFilters(filtersObject);
+  }
+  catch (error) {
+    console.error(error);
+  }
+  
+}
+</script>
+
+<template >
+  <HeaderComponent />
+  <div class="bg-gray-200 p-4 min-h-screen">
+  <Filters @filter="handleFilters"/>
+  <Loading  v-if="legendStore.loading" class="flex justify-center"/>
+  <Error v-if="legendStore.error" :error="legendStore.error ?? 'An error know occurred'" />
+  <section class="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 " v-if="!legendStore.loading && !legendStore.error">
+    <CardLegend
+      v-for="legend in legendStore.legends"
+      :key="legend.id"
+      :id="legend.id"
+      :name="legend.name"
+      :category_name="legend.category_name"
+      :description="legend.description"
+      :imageUrl="legend.image_url"
+      :legend_date="legend.legend_date"
+      :district_name="legend.district_name"
+      :canton_name="legend.canton_name"
+      :province_name="legend.province_name"
+    />
+    <p v-if="legendStore.legends.length === 0"> No legends found</p>
+  </section>
+</div>
+
+
 </template>
---
+++ src/views/NotFound.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+    import Button from '../components/ui/Button.vue'
+</script>
+
+<template>
+    <div class="flex flex-col items-center justify-center h-screen">
+        <h1 class="text-9xl text-primary font-bold" >404</h1>
+        <h2 class="text-6xl text-primary font-bold">Page not found</h2>
+        <Button class="hover:scale-105 text-xl mt-3 border bg-primary text-white"
+        ><router-link to="/">Back to Home</router-link></Button>
+    </div>
+</template>
